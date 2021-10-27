a887a506dd75d63192ed963b1818ba8a628371ec
Atul Patil <atulpatil539@gmail.com>
2021-10-27 17:37:16 +0530

    feat : intial commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyDbRW9_F--MQdhOXZ6IUNfEBrcw92fZCsI",
+  authDomain: "superchat-81638.firebaseapp.com",
+  projectId: "superchat-81638",
+  storageBucket: "superchat-81638.appspot.com",
+  messagingSenderId: "263768869536",
+  appId: "1:263768869536:web:aa969dfc327082d40357c5"
+})
+
+const db = firebaseApp.firestore()
+
+
+const auth = firebase.auth()
+
+export { db, auth }
