1dd66a47015895571cc2a21a678881534d918b7f
Atul Patil <atulpatil539@gmail.com>
2021-10-27 19:16:54 +0530

    feat : added Auth

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import firebase from 'firebase'
+import { auth } from '../firebase.js'
+// import { Button } from '@material-ui/core'
+import Button from '@material-ui/core/Button';
 
 function SignIn() {
+   function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+   }
     return (
         <div>
-            
+          <Button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</Button>  
         </div>
     )
 }
