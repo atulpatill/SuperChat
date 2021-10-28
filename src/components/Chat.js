cdf43c833da577602eacde8c7a825c0e624729fd
Atul Patil <atulpatil539@gmail.com>
2021-10-28 09:47:52 +0530

    feat: chat messages stored in firebase

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+import { db } from '../firebase'
+import SendMessage from './SendMessage'
+import SignOut from './SignOut'
 
 function Chat() {
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+     db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))
+  })
+  },[])
     return (
         <div>
-          Chat  
+          <SignOut />
+          {messages.map(({ id, text, photoURL, uid }) => (
+     <div>
+     <div key={id} >
+         <img src={photoURL} alt="" />
+         <p>{text}</p>
+     </div>
+ </div>
+))} 
+<SendMessage />
         </div>
     )
 }
